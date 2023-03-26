fd61704e76ad92ca4c37cc6b68fd240c18c21d35
imhimansu28 <imhimansu28@gmail.com>
2023-03-26 21:00:54 +0530

    fix messages templates in login and register page

--- tasksmanager/settings/base.py
+++ tasksmanager/settings/base.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 
 import environ
+from django.contrib.messages import constants as messages
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
@@ -162,3 +163,12 @@ STATICFILES_DIRS = (BASE_DIR / "www",)
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+MESSAGE_TAGS = {
+    messages.DEBUG: "alert-secondary",
+    messages.INFO: "alert-info",
+    messages.SUCCESS: "alert-success",
+    messages.WARNING: "alert-warning",
+    messages.ERROR: "alert-danger",
+}
